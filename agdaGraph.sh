c9c93f9b026ff9f550b7b41eeddc77e9d577d5c4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-01-24 15:49:00 +0100

    Revert "remove agdaGraph.sh for deployment (tillmann's name's in it)"
    This reverts commit 9688e81dfcdc32cc645ea964710fe040749a0c2b.
    But now we can deanonymize the paper's artifacts.
    
    Old-commit-hash: b067b4d3f9de62ea9221a30ff2ebc7588f8c7135

---
+++ agdaGraph.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+. agdaConfParse.sh.inc
+
+if ! which filter-agda-dependency-graph > /dev/null; then
+  cat <<-EOF
+	To run this script, install Tillmann's filter-agda-dependency-graph from
+	Github, with something like:
+
+	$ git clone https://github.com/Toxaris/filter-agda-dependency-graph.git
+	$ cd filter-agda-dependency-graph
+	$ cabal install -v --dry-run
+	# Check that the installation plan makes sense, as usual with cabal
+	# install.
+	$ cabal install
+
+	Exiting.
+EOF
+  exit 1
+fi
+
+if ! which dot > /dev/null; then
+  cat <<-EOF
+	To run this script, install GraphViz with your favorite package manager.
+	Only the \`dot\` program is required.
+	Exiting.
+EOF
+  exit 1
+fi
+
+agda -i . -i ${AGDA_LIB} ${mainFile} --dependency-graph big.dot
+filter-agda-dependency-graph < big.dot > small.dot
+dot -Tpdf small.dot > small.pdf
