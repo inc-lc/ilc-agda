inc-lc/ilc-agda

commit 49e8b38c23e9c250b738a0ddba6384e6268034bd

modernize and anonymize README.agda and EVERYTHING.agda
Old-commit-hash: 7a50fb4467922ac611488c1c25dc8ca7b2f2e47e

README.agda
@@ -1,19 +1,37 @@
 module README where
 
--- INCREMENTAL λ-CALCULUS
---   with total derivatives
+----------------------------
+-- INCREMENTAL λ-CALCULUS --
+----------------------------
 --
--- Features:
---   * changes and derivatives are unified (following Cai)
---   * multiple calculi
+--
+-- IMPORTANT FILES
+--
+-- README.agda
+--   This file. A coarse-grained introduction to the Agda formalization.
+--
+-- PLDI14-List-of-Theorems.agda
+--   For each theorem, lemma or definition in the PLDI 2014 submission,
+--   it points to the corresponding Agda object.
+--
+--
+-- LOCATION OF AGDA MODULES
+--
+-- To find the file containing an Agda module, replace the dots in its
+-- full name by directory separators. The result is the file's path relative
+-- to this directory. For example, Parametric.Syntax.Type is defined in
+-- Parametric/Syntax/Type.agda.
+--
+--
+-- HOW TO TYPE CHECK EVERYTHING
 --
 -- To typecheck this formalization, you need to install the appropriate version
 -- of Agda, the Agda standard library (version 0.7), generate Everything.agda
 -- with the attached Haskell helper, and finally run Agda on this file.
 --
 -- Given a Unix-like environment (including Cygwin), running the ./agdaCheck.sh
--- script and following instructions given on output will help with the
--- "generate Everything.agda" part.
+-- script and following instructions given on output will eventually generate
+-- Everything.agda and proceed to type check everything on command line.
 --
 -- We use Agda HEAD from September 2013; Agda 2.3.2.1 might happen to work, but
 -- has some bugs with serialization of code using some recent syntactic sugar
@@ -100,42 +118,3 @@ import Popl14.Change.Validity
 import Popl14.Change.Specification
 import Popl14.Change.Implementation
 import Popl14.Change.Correctness
-
--- Some other calculus (remove from here?)
-
-import Atlas.Syntax.Type
-import Atlas.Syntax.Term
-import Atlas.Change.Type
-import Atlas.Change.Term
-import Atlas.Change.Derive
-
--- Import everything else.
-import Everything
-
--- XXX: Do we still care about these?
-
-import Property.Uniqueness
-
-import Structure.Bag.Popl14
-import Structure.Tuples
-
-import Theorem.CongApp
-import Theorem.EqualityUnique
-import Theorem.Groups-Popl14
-import Theorem.Irrelevance-Popl14
-import Theorem.IrrelevanceUnique-Popl14
-import Theorem.ProductUnique
-
--- Old stuff.
--- XXX Update and integrate those descriptions where appropriate.
-
-{-
--- Correctness theorem for canonical derivation
-import Popl14.Change.Correctness
-
--- Denotation-as-specification for canonical derivation
-import Popl14.Change.Specification
-
--- The idea of implementing a denotational specification
-import Popl14.Change.Implementation
--}

EverythingHeader.agda.inc
@@ -3,7 +3,7 @@
 --
 -- Features:
 --   * total derivatives
---   * changes and derivatives are unified (following Cai)
+--   * changes and derivatives are unified
 --   * multiple calculi
 --
 -- All library modules, along with (optional) short descriptions

commit 1fde24fb24ccef8f08e65f7497a7ec3cd7eee960

Ensure README.agda imports everything (fix #41)
This was broken in 49e8b38c23e9c250b738a0ddba6384e6268034bd, so add a
comment explaining why that line needs to be there.

I verified that the code still typechecks on my machine.

Old-commit-hash: 36ff398aab5149e6b6845d2c6ea1f05ab8d97bf8

README.agda
@@ -122,3 +122,7 @@ import Nehemiah.Change.Validity
 import Nehemiah.Change.Specification
 import Nehemiah.Change.Implementation
 import Nehemiah.Change.Correctness
+
+-- Import everything else. This ensures that typechecking README.agda typechecks
+-- the entire codebase, because Everything.agda is auto-generated.
+import Everything